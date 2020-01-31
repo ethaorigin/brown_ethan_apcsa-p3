98b3c7bf80f41baba83e0d7d8a3800ed7df0ad8f
ethaorigin <ethaorigin@gmail.com>
2020-01-31 13:30:46 -0800

    added StarsRunner and made it actually work

--- Unit01/src/StarsAndStripes.java
+++ Unit01/src/StarsAndStripes.java
@@ -6,38 +6,58 @@
 //Class -
 //Lab  -
 
-import static java.lang.System.*;
 
-public class StarsAndStripes
-{
-   public StarsAndStripes()
-   {
-      out.println("StarsAndStripes");
-      printTwoBlankLines();
-   }
+		import static java.lang.System.*;
+		
+		public class StarsAndStripes
+		{
+		   public StarsAndStripes()
+		   {
+		      out.println("StarsAndStripes");
+		      printTwoBlankLines();
+		   }
 
-   public void printTwentyStars()
-   {
-	   out.println("********************");
-   }
+		   public void printTwentyStars()
+		   {
+			   out.println("********************");
+		   }
 
-   public void printTwentyDashes()
-   {
-	   out.println("--------------------");
-   }
+		   public void printTwentyDashes()
+		   {
+			   out.println("--------------------");
+		   }
 
-   public void printTwoBlankLines()
-   {
-	   out.println("\n\n");
-   }
-   
-   public void printASmallBox()
-   {	
-	   out.println("********************");
-   }
- 
-   public void printABigBox()
-   { 	
-	   
-   }   
-}
+		   public void printTwoBlankLines()
+		   {
+			   out.println("\n\n");
+		   }
+		   
+		   public void printASmallBox()
+		   {	
+			   out.println("--------------------");
+			   out.println("********************");
+			   out.println("--------------------");
+			   out.println("********************");
+			   out.println("--------------------");
+			   out.println("********************");
+			   out.println("--------------------");
+		   }
+		 
+		   public void printABigBox()
+		   { 	
+			   out.println("--------------------");
+			   out.println("********************");
+			   out.println("--------------------");
+			   out.println("********************");
+			   out.println("--------------------");
+			   out.println("********************");
+			   out.println("--------------------");
+			   out.println("--------------------");
+			   out.println("********************");
+			   out.println("--------------------");
+			   out.println("********************");
+			   out.println("--------------------");
+			   out.println("********************");
+			   out.println("--------------------");
+		   }   
+		}
