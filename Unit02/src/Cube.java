ca0c89392147f2ea17dde19af9ae26cf4a54d664
ethaorigin <ethaorigin@gmail.com>
2020-02-03 12:59:27 -0800

    finished unit2 and added unit3

---
+++ Unit02/src/Cube.java
@@ -0,0 +1,14 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+//Name -
+//Date -
+
+public class Cube
+{
+	public static double area( int side )
+	{
+		side = (int) Math.pow(side, 2);
+		side = side*6;
+		return side;
+	}
+}
